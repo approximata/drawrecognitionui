0813c185a830d4afe19afc0fd7f7520c204a6abf
borosdenes <boros.denes@gmail.com>
2019-11-28 19:07:38 +0100

    Insert meaningful web developer commit message here
    approximata made the changes, I have nothing to do with web development

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 const port = 3000
 
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 app.use(cors())
-app.use(express.static('model'))
-app.listen(port, () => console.log(`Model folder is avaiable at port: ${port}!`))
+app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/model'))
+app.listen(port, () => console.log(`Server listening on: ${port} port!`))
